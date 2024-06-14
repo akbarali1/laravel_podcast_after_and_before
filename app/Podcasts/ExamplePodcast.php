b66562559e01c8828d3c395b850dc981014b50b5
Akbarali <39323182+akbarali1@users.noreply.github.com>
2024-06-14 16:28:26 +0500

    end

--- app/Podcasts/ExamplePodcast.php
+++ app/Podcasts/ExamplePodcast.php
@@ -6,15 +6,19 @@ namespace App\Podcasts;
 final class ExamplePodcast extends ManagementPodcast
 {
 	
+	public function beforeExecuted(): void
+	{
+		// TODO: Implement beforeExecuted() method.
+	}
+	
+	public function execute(): void
+	{
+		// TODO: Implement execute() method.
+	}
+	
 	public function afterExecute(): void
 	{
 		// TODO: Implement afterExecute() method.
 	}
 	
-
-	public function execute(): void
-	{
-		// TODO: Implement execute() method.
-	}
-
 }
--- app/Podcasts/ManagementPodcast.php
+++ app/Podcasts/ManagementPodcast.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 namespace App\Podcasts;
 
 use App\Contracts\AfterExecutePodcastsContract;
+use App\Contracts\BeforeExecutePodcastsContract;
 
-abstract class ManagementPodcast extends BasePodcast implements AfterExecutePodcastsContract
+abstract class ManagementPodcast extends BasePodcast implements AfterExecutePodcastsContract, BeforeExecutePodcastsContract
 {
 	/**
 	 * @return void
